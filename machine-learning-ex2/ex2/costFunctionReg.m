d4abb0dc4477a33501b18c6741f8b9317ced1392
yu <peacetomori1118@gmail.com>
2020-05-28 17:21:37 +0900

    finishing hw2

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,6 +17,38 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+m = size(X, 1);
+n = size(theta, 1);
+eachCost = zeros(m, 1);
+simpleCost = zeros(m, 1);
+grad = zeros(theta, 1);
+
+for i = 1:m
+    sigValue = sigmoid(X(i,:) * theta);
+    eachCost(i) = -y(i) * log(sigValue) - (1 - y(i)) * log(1 - sigValue);
+    simpleCost(i) = sigmoid(X(i, :) * theta) - y(i);
+end
+
+y = 0;
+for i = 2:n
+    y += theta(i) ^ 2;
+end
+J = sum(eachCost) / m + (lambda / (2 * m)) * y;
+
+y = 0;
+for i = 1:m
+    y += simpleCost(i) * X(i, 1);
+end
+grad(1) = y / m;
+
+for j = 2:n
+    y = 0;
+    for i = 1:m
+        y += simpleCost(i) * X(i, j);
+    end
+    grad(j) = y / m + lambda * theta(j) / m;
+end
+
 
 
 
